b96c9810c22538ecfa7ebdab9264d9fc2794bd7f
nadeemtadvi <nadeemtadvi1@gmail.com>
2024-12-01 03:21:01 +0530

    create textimonial section

---
+++ main.js
@@ -0,0 +1,10 @@
+const swiper = new Swiper(".swiper", {
+  loop: true,
+  autoplay: false,
+  slidesPerView: 4,
+  spaceBetween: 5,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
