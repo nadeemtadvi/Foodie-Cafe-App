4af0e503bf2b76a75a3d8dbf674f8d7298e247d6
nadeemtadvi <nadeemtadvi1@gmail.com>
2024-12-02 00:32:28 +0530

    optimize image and set

--- main.js
+++ main.js
@@ -1,10 +1,24 @@
 const swiper = new Swiper(".swiper", {
   loop: true,
   autoplay: false,
-  slidesPerView: 4,
+  slidesPerView: 1,
   spaceBetween: 5,
   pagination: {
     el: ".swiper-pagination",
     clickable: true,
+  },
+  breakpoints: {
+    1100: {
+      slidesPerView: 4,
+      spaceBetween: 10,
+    },
+    890: {
+      slidesPerView: 3,
+      spaceBetween: 10,
+    },
+    468: {
+      slidesPerView: 2,
+      spaceBetween: 5,
+    },
   },
 });
